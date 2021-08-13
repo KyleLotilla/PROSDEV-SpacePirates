a8de9020692c4fd9f82b4ddbd877416b1c456dc5
KyleLotilla <kyle_lotilla@dlsu.edu.ph>
2021-08-13 22:43:33 +0800

    Added Initial Enemy Spawning System, Level System & Utility ScriptableObjects
    Enemy Spawning System includes the following:
    - Enemy Database
    - Encounters
    - Strength
    - Progression
    
    Level System includes:
    - Support for Serialization and Deserialization
    - Initialization
    
    The following Utility ScriptableObjects were made:
    - IntVariable
    - FloatVariable
    - GameEvent

---
+++ Assets/Game/EnemySpawn/Scripts/Encounter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DLSU.SpacePirates.EnemySpawn
+{
+    [Serializable]
+    public class Encounter
+    {
+        [SerializeField]
+        private float initialStrength;
+
+        public float InitialStrength
+        {
+            get
+            {
+                return initialStrength;
+            }
+            set
+            {
+                initialStrength = value;
+            }
+        }
+        [SerializeField]
+        private float maxStrength;
+        public float MaxStrength
+        {
+            get
+            {
+                return maxStrength;
+            }
+            set
+            {
+                maxStrength = value;
+            }
+        }
+        [SerializeField]
+        private float duration;
+        public float Duration
+        {
+            get
+            {
+                return duration;
+            }
+            set
+            {
+                duration = value;
+            }
+        }
+        [SerializeField]
+        private List<EnemySpawnInstance> enemySpawns;
+
+        public List<EnemySpawnInstance> EnemySpawns
+        {
+            get
+            {
+                return enemySpawns;
+            }
+            set
+            {
+                enemySpawns = value;
+            }
+        }
+    }
+}
+
---
+++ Assets/Game/Level/Scripts/LevelVariable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DLSU.SpacePirates.Level
+{
+    [CreateAssetMenu(menuName = "ScriptableObjects/LevelVariable")]
+    public class LevelVariable : ScriptableObject
+    {
+        [SerializeField]
+        private Level currentValue;
+        public Level Value
+        {
+            get
+            {
+                return currentValue;
+            }
+            set
+            {
+                currentValue = value;
+            }
+        }
+
+        private void OnEnable()
+        {
+            currentValue = null;
+        }
+    }
+}
+
