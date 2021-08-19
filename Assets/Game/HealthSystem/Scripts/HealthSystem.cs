06c5ea7eff98409eb70693f7d856c8b3d6564240
Warren Duhaylungsod <paol_duhaylungsod@dlsu.edu.ph>
2021-08-19 21:45:34 +0800

    Health System and HealthBarCanvas Prefab
    Simply attach Health System script to any GameObject(player,enemy,boss) then set its health and then attach HealthBarCanvas Prefab in the Hierarchy

---
+++ Assets/Game/HealthSystem/Scripts/HealthSystem.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+    [SerializeField] private int maxHealth = 100;
+    [SerializeField] private int currentHealth;
+    [SerializeField] private HealthBar healthBar;
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    private void Update()
+    {
+
+       
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            TakeDamage(10);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.I))
+        {
+            Heal(5);
+        } 
+    }
+
+    private void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth < 0) currentHealth = 0;
+        healthBar.SetHealth(currentHealth);
+        //if collided with hostile objects reduce health
+        //if health 0 uhhh D I E. 
+    }
+
+    private void Heal(int heal)
+    {
+        currentHealth += heal;
+        if (currentHealth > maxHealth) currentHealth = maxHealth;
+        healthBar.SetHealth(currentHealth);
+        //if collided with heal power up or regeneration then increase health;
+        
+    }
+
+}
