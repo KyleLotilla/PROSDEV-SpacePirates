d73e4d31823b7b356788901e7f98844f6aa4a7bd
AplDGrape <raphaelapolinar16@yahoo.com>
2021-08-20 19:24:48 +0800

    Projectile prefab

---
+++ Assets/Game/WeaponSystem/Scripts/ProjectilePrefab.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectilePrefab : MonoBehaviour
+{
+    public float speed = 10f;
+    public Rigidbody2D rb;
+
+    //public GameObject impactEffect;
+
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Instantiate(impactEffect, transform.position, transform.rotation);
+
+        Destroy(gameObject);
+    }
+}
