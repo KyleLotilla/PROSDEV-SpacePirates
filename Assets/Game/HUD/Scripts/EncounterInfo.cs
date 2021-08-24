8d4a617cbda1233a6d4fdd9738414707735c03d0
marcomalabag <46800870+marcomalabag@users.noreply.github.com>
2021-08-24 17:54:55 +0800

    - Added the initial UI for encounter info and weapon info

---
+++ Assets/Game/HUD/Scripts/EncounterInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DLSU.SpacePirates.Level;
+using DLSU.SpacePirates.Util;
+using TMPro;
+
+
+public class EncounterInfo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private IntVariable currentencountercount;
+    
+    [SerializeField]
+    private LevelVariable MaxEncounter;
+
+    [SerializeField]
+    public TextMeshProUGUI CurrentEncounter;
+
+    [SerializeField]
+    public TextMeshProUGUI TotalEncounter;
+
+
+    void Start()
+    {
+        CurrentEncounter.text = currentencountercount.Value.ToString();
+        TotalEncounter.text = MaxEncounter.Value.Encounters.Count.ToString();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        CurrentEncounter.text = currentencountercount.Value.ToString();
+    }
+}
+
+
---
+++ Assets/Game/HUD/Scripts/WeaponInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DLSU.SpacePirates.WeaponSystem;
+
+
+public class WeaponInfo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    public WeaponEquipment weaponequipment;
+    
+    [SerializeField]
+    public Image Weapon;
+    
+    [SerializeField]
+    public TextMeshProUGUI Ammo;
+    
+    void Start()
+    {
+        Weapon.sprite = weaponequipment.EquippedWeapon.Sprite;
+        Ammo.text = weaponequipment.ammo.ToString();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
